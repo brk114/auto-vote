236e6761e5bb31ce919ff223f09d85ad7cc1ebdd
Ravikumar Bomma <ravib@techdimension.in>
2021-05-03 14:55:32 +0530

    Updated bat files with

---
+++ install.bat
@@ -0,0 +1,7 @@
+:: Install java runtime for internet
+
+:: installs protractor
+npm install -g protractor
+
+:: Updated webdriver manager
+webdriver-manager update
---
+++ start-voting.bat
@@ -0,0 +1,1 @@
+.\vote-sheduler\AutoVote.exe 14
