029c383cd01bbbee7d46e6651a50c29ea540acb1
Ravikumar Bomma <ravib@techdimension.in>
2021-05-03 16:08:14 +0530

    Updated code and schedule start inputs

--- start-voting.bat
+++ start-voting.bat
@@ -1,3 +1,20 @@
+@echo off
+
+set REL_PATH=.
+set ABS_PATH=
+
+rem // Save current directory and change to target directory
+pushd %REL_PATH%
+
+rem // Save value of CD variable (current directory)
+set ABS_PATH=%CD%
+
+rem // Restore original directory
+popd
+
+echo Relative path: %REL_PATH%
+echo Maps to path: %ABS_PATH%\cast-vote.bat
+
 :: The number along with AutoVote.exe is initial delay(in minutes) to start the job, 
 :: The job will execute every 30.5 minutes
-.\vote-sheduler\AutoVote.exe 2
+%ABS_PATH%\vote-sheduler\AutoVote.exe %ABS_PATH%\cast-vote.bat 1
