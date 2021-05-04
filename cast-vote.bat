11c0971b60e2e2450f68fb7f54eceefe6c9aeeea
Ravikumar Bomma <ravib@techdimension.in>
2021-05-04 19:43:52 +0530

    Removed logging to file

--- cast-vote.bat
+++ cast-vote.bat
@@ -1,17 +1,19 @@
-@echo off   
+@ECHO OFF   
 
 SET HOUR=%time:~0,2%
 SET dtStamp9=%date:~-4%-%date:~4,2%%date:~0,2%_0%time:~1,1%-%time:~3,2%-%time:~6,2% 
 SET dtStamp24=%date:~-4%-%date:~4,2%%date:~0,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
 
-if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
+IF "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) ELSE (SET dtStamp=%dtStamp24%)
 
 ECHO %date% %time%
-ECHO %dtStamp%
+:: ECHO %dtStamp%
 
-SET filename=%ABS_PATH%\logs\log_%dtStamp%.log 2>&1
-set filename=%filename: =%
-echo %filename%
-protractor %ABS_PATH%\conf.js > %filename%
+:: SET filename=%ABS_PATH%\logs\log_%dtStamp%.log 2>&1
+:: SET filename=%filename: =%
+:: ECHO %filename%
 
-goto :eof
+protractor %ABS_PATH%\conf.js 
+::protractor %ABS_PATH%\conf.js > %filename%
+ECHO Voting completed at: %date% %time%
+GOTO :EOF
