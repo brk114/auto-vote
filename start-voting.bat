ca9936ff1c69b9739008dab677096b5c043f5aea
Ravikumar Bomma <ravib@techdimension.in>
2021-05-04 20:15:34 +0530

    Updated project paths with env variables

--- start-voting.bat
+++ start-voting.bat
@@ -21,14 +21,15 @@ IF "%arg1%"=="" set "arg1=0"
 echo Initial Delay(in minutes): %arg1%
 :: The number along with AutoVote.exe is initial delay(in minutes) to start the job, 
 :: The job will execute every 30.5 minutes
-SET PROJECT_PATH = %ABS_PATH%\AutoVoteScheduler\AutoVote
-SET SCHEDULER_EXE_PATH = %PROJECT_PATH%\bin\Debug\netcoreapp3.1
+SET SLN_PATH=%ABS_PATH%\AutoVoteScheduler
+SET SCHEDULER_EXE_PATH="%SLN_PATH%\AutoVote\bin\Debug\netcoreapp3.1"
 
+ECHO Checking scheduler exe path at %SCHEDULER_EXE_PATH%
 IF NOT EXIST "%SCHEDULER_EXE_PATH%\AutoVote.exe" (
-    dotnet build %PROJECT_PATH%
+    dotnet build %SLN_PATH%
 )
 
 :: starting web driver
 start cmd /k Call start-webdriver.bat
 :: starting the auto vote scheduler
-%EXE_PATH%\AutoVote.exe %ABS_PATH%\cast-vote.bat %arg1%
+%SCHEDULER_EXE_PATH%\AutoVote.exe %ABS_PATH%\cast-vote.bat %arg1%
